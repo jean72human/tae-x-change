53a30b153e52713afdf9e6ad64184e970d543755
jean72human <jsadovonon@gmail.com>
2018-04-29 23:04:17 +0000

    improve api

--- tae-api.py
+++ tae-api.py
@@ -4,7 +4,7 @@ from flask.ext.api.decorators import set_renderers
 from flask.ext.api.renderers import JSONRenderer
 from flask import Blueprint
 
-from dbConnector import connect, login
+from dbConnector import connect, login, register
 
 
 
@@ -24,8 +24,26 @@ def login_route():
     conn = connect()
     if request.method == 'POST':
         answer = login(conn, request.data.get('user_email'), request.data.get('password'))
+        conn.close()
         return answer
+    conn.close()
     return {'message':'post your credentials'}
+
+@app.route("/register", methods=['POST','GET'])
+def register_route():
+    conn = connect()
+    if request.method == 'POST':
+        answer = register(conn, request.data.get('email'), request.data.get('password'), request.data.get('phone_number'))
+        conn.close
+        return answer
+    return {'message':'Enter the info to register'}
+
+@app.route("/",methods=['GET'])
+def home():
+    return {
+        '127.0.0.1:5000/login':'login',
+        '127.0.0.1:5000/register':'register'
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
